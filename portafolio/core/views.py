c5de3b2e43a07e81b295e7d11d4d953595c15d07
dabrianla <81344554+dabrianla@users.noreply.github.com>
2026-03-31 18:53:14 -0300

    Rename home view to index and update template

--- portafolio/core/views.py
+++ portafolio/core/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def home(request):
-    return render(request, 'core/home.html')
+def index(request):
+    return render(request, 'core/index.html')
